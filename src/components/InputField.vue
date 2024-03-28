ec17b43a85b07712ca61bad2eed1df7140b330e8
Jazmine Tran <jazmine.tran@oakstreethealth.com>
2024-03-28 14:11:51 -0500

    created methods, mounted, and reworked options

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,41 +1,50 @@
 <template>
-  <form @submit.prevent="getConversion">
+  <form @submit.prevent="conversionRate">
     <div>
       <input type="text" v-model="amountInput" />
       <select name="currency" id="currency" v-model="currencyOne">
-        <option v-for="key in currencyKeys" :key="key" value="key">
+        <option v-for="key in currencyKeys" :key="key">
           {{ key }}
         </option>
       </select>
     </div>
     <p>=</p>
     <div>
-      <input type="text" disabled />
+      <input type="text" :value="conversion" disabled />
       <select name="currency" id="currency" v-model="currencyTwo">
-        <option v-for="key in currencyKeys" :key="key" value="key">
+        <option v-for="key in conversionKeys" :key="key">
           {{ key }}
         </option>
       </select>
+      <button>Convert</button>
     </div>
   </form>
+  <!-- <h3>Test API Here</h3>
+  <button @click="testAPI">Test</button> -->
 </template>
 
 <script>
 export default {
   data() {
     return {
-      data: [],
+      data: {},
       currencyKeys: [],
+      conversionKeys: [],
+      specificCurrency: [],
       currencyOne: "",
-        currencyTwo: "",
-      amountInput: ""
+      currencyTwo: "",
+      amountInput: "",
+      sampleCurrency: "XAU",
+      testData: "",
+      conversion: "",
     };
   },
   methods: {
-    fetchResponse() {
+    fetchData() {
       fetch("https://api.bitpanda.com/v1/ticker")
         .then((response) => response.json())
         .then((data) => (this.data = data));
+      console.log(this.data);
     },
     getCurrencyKeys() {
       fetch("https://api.bitpanda.com/v1/ticker")
@@ -44,14 +53,33 @@ export default {
           this.currencyKeys = Object.keys(data);
         });
     },
-      conversionRate() { },
-      getConversion() {
-        const rate =
-    }
+    getConversionKeys() {
+      fetch("https://api.bitpanda.com/v1/ticker")
+        .then((response) => response.json())
+        .then((data) => {
+          this.conversionKeys = Object.keys(data.BTC);
+        });
+    },
+    getSpecificCurrency() {
+      fetch("https://api.bitpanda.com/v1/ticker")
+        .then((response) => response.json())
+        .then((data) => {
+          this.specificCurrency = data.BTC;
+        });
+    },
+    conversionRate() {
+      const rate = this.data[this.currencyOne][this.currencyTwo];
+      this.conversion = parseFloat(this.amountInput) * rate;
+    },
+    testAPI() {
+      console.log(this.currencyKeys);
+    },
   },
   mounted() {
     this.getCurrencyKeys();
     this.fetchData();
+    this.getSpecificCurrency();
+    this.getConversionKeys();
   },
 };
 </script>
